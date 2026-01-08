a5cef78babe6be12c2c6654cc9154cc17d0a947f
Jacob Zwack <jakezwack@gmail.com>
2026-01-08 18:00:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3405140663-7dfb5",
+  "appId": "1:673362725679:web:b7fc4cd66f9311c8516fda",
+  "apiKey": "AIzaSyDa4AjBrKsSuLLtpYtLlvE8Skh0tSQYXHc",
+  "authDomain": "studio-3405140663-7dfb5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "673362725679"
+};
